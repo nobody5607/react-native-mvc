6d6189bc1747e8eeb7f18d26fb31920bd1a364da
NuttaphonChanpan <chanpan.nuttaphon@gmail.com>
2022-07-14 04:57:15 +0700

    card

--- src/screens/product/product.screen.tsx
+++ src/screens/product/product.screen.tsx
@@ -1,25 +1,34 @@
-import { ActivityIndicator, FlatList, Image, RefreshControl, SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { ActivityIndicator, Dimensions, FlatList, Image, RefreshControl, SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import React from 'react';
 import ProductController from '@controllers/product.controller';
 import { Product } from '@models/product.model';
 import colors from '@themes/colors';
 import fonts from '@themes/fonts';
+import { scale } from 'react-native-size-matters';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const ProductScreen = () => {
-    const isDarkMode = useColorScheme() === 'light';
+    const isDarkMode = useColorScheme() === 'dark'; //'light','dark'
+
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
     const { products, loading, error, refreshing, count, onRefresh, isCloseToBottom, loadMore } = ProductController();
 
 
     const ProductItems = ({ product }: { product: Product; }) => (
-        <View style={styles.productItems}>
-            <Image style={styles.tinyLogo} source={{ uri: product.image }} />
-            <Text style={styles.productNameText}>{product.product_name}</Text>
+        <View style={isDarkMode ? styles.productCartDark : styles.productCartLight}>
+            <View style={styles.containerImage}>
+                <Image style={styles.productImage} source={{ uri: product.image }} />
+            </View>
+            <Text style={isDarkMode ? styles.productNameTextDark : styles.productNameTextLight}>{product.product_name}</Text>
             <Text style={styles.productPrice}>฿{product.price}</Text>
         </View>
     );
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={backgroundStyle}>
             <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
             <Text>จำนวนสินค้าทั้งหมด {count} รายการ</Text>
 
@@ -45,26 +54,56 @@ const ProductScreen = () => {
 };
 export default ProductScreen;
 const styles = StyleSheet.create({
-    productItems: {
-        backgroundColor: colors.while,
+    productCartLight: {
+        backgroundColor: Colors.lighter,
         borderRadius: 10,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        flex: 1
+        marginVertical: 15,
+        marginHorizontal: 15,
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden'
     },
-    tinyLogo: {
+    productCartDark: {
+        backgroundColor: Colors.darker,
+        borderRadius: 10,
+        marginVertical: 15,
+        marginHorizontal: 15,
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden'
+    },
+    containerImage: {
+        width: scale(150),
+        height: scale(140),
+    },
+    productImage: {
         width: '100%',
-        height: 150,
+        height: '100%',
+        resizeMode: "cover",
+        flex: 1,
+
     },
-    productNameText: {
+    productNameTextDark: {
         fontSize: fonts.fontSize.font16,
-        color: colors.black,
+        color: Colors.lighter,
+        padding: 10
+    },
+    productNameTextLight: {
+        fontSize: fonts.fontSize.font16,
+        color: Colors.darker,
         padding: 10
     },
     productPrice: {
         color: colors.primary,
         fontSize: fonts.fontSize.font18,
         fontWeight: fonts.fontWeight.bold,
-
+        marginBottom: 10,
+        marginTop: 10,
     }
 });
